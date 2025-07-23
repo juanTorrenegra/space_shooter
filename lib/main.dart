0663b16a52e49eb6b2ea13c02ef2a5bbe4ca9c7c
juan <juantorrenegra2000@gmail.com>
2025-07-22 20:16:21 -0500

    add class Explosion extends SpriteAnimationComponent

--- lib/main.dart
+++ lib/main.dart
@@ -180,6 +180,32 @@ class Enemy extends SpriteAnimationComponent
     if (other is Bullet) {
       removeFromParent();
       other.removeFromParent();
+      game.add(Explosion(position: position));
     }
+  }
+}
+
+class Explosion extends SpriteAnimationComponent
+    with HasGameReference<SpaceShooterGame> {
+  Explosion({super.position})
+    : super(
+        size: Vector2.all(150),
+        anchor: Anchor.center,
+        removeOnFinish: true,
+      );
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    animation = await game.loadSpriteAnimation(
+      'explosion.png',
+      SpriteAnimationData.sequenced(
+        amount: 6,
+        stepTime: .1,
+        textureSize: Vector2.all(32),
+        loop: false,
+      ),
+    );
   }
 }
